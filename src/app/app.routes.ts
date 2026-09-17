eb1d5fdd19b59861bfc94c78b816414ac743c811
julioroodriguez <jrodriguezcanovas@gmail.com>
2026-09-17 11:35:31 +0200

    feat: add application layout and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/pages/home/home.component';
+import { SearchComponent } from './features/search/pages/search/search.component'; 
+import { MovieDetailComponent } from './features/movie/pages/movie-detail/movie-detail.component'; 
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+    },
+     {
+        path: 'search',
+        component: SearchComponent,
+    },
+     {
+        path: 'movie/:id',
+        component: MovieDetailComponent,
+    },
+     {
+        path: '**',
+        redirectTo: '',
+    },
+];
