de98ea9677ec035a6bbcdb607dd51934f1cf023d
julioroodriguez <jrodriguezcanovas@gmail.com>
2026-09-22 14:37:13 +0200

    feat: display movie sections on home page

--- src/app/features/home/pages/home/home.component.ts
+++ src/app/features/home/pages/home/home.component.ts
@@ -1,47 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { finalize } from 'rxjs';
+import { finalize, forkJoin } from 'rxjs';
 
 import { Movie } from '../../../../core/models/movie.model';
 import { MovieService } from '../../../../core/services/movie.service';
-
+import { MovieSectionComponent } from '../../../../shared/components/movie-section/movie-section.component';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, MovieSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit{
 
-  movies: Movie[] = [];
+ popularMovies: Movie[] = [];
+  trendingMovies: Movie[] = [];
+  upcomingMovies: Movie[] = [];
+
   loading = false;
   errorMessage = '';
 
   constructor(private movieService: MovieService){}
 
   ngOnInit(): void {
-      this.loadPopularMovies();
+      this.loadHomeMovies();
   }
 
-  loadPopularMovies(): void{
+  loadHomeMovies(): void{
     this.loading = true;
     this.errorMessage = '';
 
-    this.movieService.getPopularMovies().pipe(
-      finalize(()=> this.loading = false)
-    ).subscribe(
-      {
-        next: (response) => {
-          this.movies = response.results;
-        },
-        error: (error) => {
-          console.error('Error loading popular movies:', error);
-          this.errorMessage = 'Could not load movies.';
-        }
+    forkJoin({
+      popular: this.movieService.getPopularMovies(),
+      trending: this.movieService.getTrendingMovies(),
+      upcoming: this.movieService.getUpcomingMovies()
+    }).pipe(
+      finalize(() => this.loading = false)
+    ).subscribe({
+      next: ({ popular, trending, upcoming }) => {
+        this.popularMovies = popular.results;
+        this.trendingMovies = trending.results;
+        this.upcomingMovies = upcoming.results;
+      },
+      error: (error) => {
+        console.error('Error loading home movies', error);
+
+        this.errorMessage = 'No se pudieron cargar las peliculas';
       }
-    );
+    });
   }
+
 }
